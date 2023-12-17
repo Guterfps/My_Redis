c22863328f1fa061b2d9881729595d1dcb8b5b72
itay.guter <ithayg@gmail.com>
2023-12-17 17:17:00 +0200

    channels

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -54,14 +54,3 @@ async fn process(socket: TcpStream, db: Db) {
         connection.write_frame(&response).await.unwrap();
     }
 }
-
-type ShardedDb = Arc<Vec<Mutex<HashMap<String, Vec<u8>>>>>;
-
-fn new_sharded_db(num_shards: usize) -> ShardedDb {
-    let mut db = Vec::with_capacity(num_shards);
-    for _ in 0..num_shards {
-        db.push(Mutex::new(HashMap::new()));
-    }
-
-    Arc::new(db)
-}
